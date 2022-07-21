c406515b19072cc68178106c2d3442a3c8288fab
Superflows <108924653+Superflow-3@users.noreply.github.com>
2022-07-21 19:16:20 +0530

    default theme added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { Constants } from './constants';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+
+function getTheme(name) {
+
+  var theme;
+
+  switch(name) {
+    case Constants.DEFAULT:
+    theme = {
+      dangerDarkColor: '#CC3300',
+      dangerLightColor: '#FFD9CC',
+      successDarkColor: '#009900',
+      successLightColor: '#E6FFE6',
+      infoDarkColor: '#4D4D4D',
+      infoLightColor: '#D9D9D9',
+      primaryButtonDarkColor: '#7300E6',
+      primaryButtonLightColor: '#FFFFFF',
+      linkButtonDarkColor: '#000000',
+      linkButtonLightColor: 'transparent'
+    }
+    break;
+  }
+
+  return theme;
+
 }
+
+const exportFunctions = {
+  getTheme
+};
+
+export default exportFunctions;
